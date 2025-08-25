4926abc470e736bb582d3f9a863f6089fbe40ba4
Inkeun Yoo <inkun00@gmail.com>
2025-08-25 01:41:40 +0000

    auth-helpers.ts:32  POST https://identitytoolkit.googleapis.com/v1/acco

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBtSox7Fg_pMG7b24BhxFaa9gt0qZ2iNcQ",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "dinorun-math-c599c.firebaseapp.com",
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "dinorun-math-c599c",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "dinorun-math-c599c.appspot.com",
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "35446643543",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:35446643543:web:fb92514e65a9b85fac3c12"
+    apiKey: "AIzaSyBtSox7Fg_pMG7b24BhxFaa9gt0qZ2iNcQ",
+    authDomain: "dinorun-math-c599c.firebaseapp.com",
+    projectId: "dinorun-math-c599c",
+    storageBucket: "dinorun-math-c599c.appspot.com",
+    messagingSenderId: "35446643543",
+    appId: "1:35446643543:web:fb92514e65a9b85fac3c12"
 };
 
 // Initialize Firebase
